f8af538d753dec4aa595d31f27a9cd4cbe5711ef
TheCooliestGuy <mflavalley@gmail.com>
2021-12-17 14:44:12 -0500

    Finished up. Need to add to ReadMe and gemfile

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// Import all plugins
-import * as bootstrap from 'bootstrap';
+// This is the javascript file containing all the functions for the button presses
 
+// Set two event listeners to the first button
 document.getElementById("Attack1").addEventListener("click", printAttack1);
 document.getElementById("Attack1").addEventListener("click", printWAF1);
 
+//Simulate the Malicious users actions for the first attack
 function printAttack1() {
-    let actions = ['End', 'Wait for victim to click link','Embed malicious request into hyperlink','Forge malicious request'];
+    let actions = ['Forge malicious request', 'Wait for victim to click link','Embed malicious request into hyperlink','End'];
 
     let nodes = actions.map(action => {
         let li = document.createElement('li');
@@ -15,11 +16,11 @@ function printAttack1() {
 
     let log = document.getElementById('User_log_text');
     log.append(...nodes);
-
+    log.scrollTo(0, 10000000); // y is very large to counter the buttons being pressed many times
 }
-
+//Simulate the Malicious users actions for the first attack
 function printWAF1() {
-    let actions = ['End', 'Present CAPTCHAs that alert unwary users','Track possible exploit attempts','Filter requests to sensitive pages'];
+    let actions = ['Filter requests to sensitive pages', 'Present CAPTCHAs that alert unwary users','Track possible exploit attempts','End'];
 
     let nodes = actions.map(action => {
         let li = document.createElement('li');
@@ -29,13 +30,16 @@ function printWAF1() {
 
     let log = document.getElementById('WAF_log_text');
     log.append(...nodes);
+    log.scrollTo(0, 10000000); // y is very large to counter the buttons being pressed many times
 }
-// Prints both logs for Attack 2: Cross-site Scripting
+
+
+// Set two event listeners to the second button
 document.getElementById("Attack2").addEventListener("click", printAttack2);
 document.getElementById("Attack2").addEventListener("click", printWAF2);
-
+//Simulate the Malicious users actions for the second attack
 function printAttack2() {
-    let actions = ['End', 'Copies cookies to impersonate other users','Inserts malicious script into an input form','Creates malicious script'];
+    let actions = ['Creates malicious script', 'Copies cookies to impersonate other users','Inserts malicious script into an input form','End'];
 
     let nodes = actions.map(action => {
         let li = document.createElement('li');
@@ -45,11 +49,11 @@ function printAttack2() {
 
     let log = document.getElementById('User_log_text');
     log.append(...nodes);
-
+    log.scrollTo(0, 10000000); // y is very large to counter the buttons being pressed many times
 }
-
+//Simulate the Malicious users actions for the second attack
 function printWAF2() {
-    let actions = ['End', 'Logs the suspected malicious request','Can allow, block, or count requests','Identifies part of web requests'];
+    let actions = ['Identifies part of web requests', 'Logs the suspected malicious request','Can allow, block, or count requests','End'];
 
     let nodes = actions.map(action => {
         let li = document.createElement('li');
@@ -59,13 +63,16 @@ function printWAF2() {
 
     let log = document.getElementById('WAF_log_text');
     log.append(...nodes);
+    log.scrollTo(0, 10000000); // y is very large to counter the buttons being pressed many times
 }
 
+
+// Set two event listeners to the third button
 document.getElementById("Attack3").addEventListener("click", printAttack3);
 document.getElementById("Attack3").addEventListener("click", printWAF3);
-
+//Simulate the Malicious users actions for the third attack
 function printAttack3() {
-    let actions = ['End', 'Inject into the input form','Create SQL query to give private information','Identify vulnerable input form'];
+    let actions = ['Identify vulnerable input form', 'Inject into the input form','Create SQL query to give private information','End'];
 
     let nodes = actions.map(action => {
         let li = document.createElement('li');
@@ -75,11 +82,11 @@ function printAttack3() {
 
     let log = document.getElementById('User_log_text');
     log.append(...nodes);
-
+    log.scrollTo(0, 10000000); // y is very large to counter the buttons being pressed many times
 }
-
+//Simulate the Malicious users actions for the third attack
 function printWAF3() {
-    let actions = ['End', 'Block and log malicious traffic','Identifies malicious traffic','Monitors POST and GET requests'];
+    let actions = ['Monitors POST and GET requests', 'Block and log malicious traffic','Identifies malicious traffic','End'];
 
     let nodes = actions.map(action => {
         let li = document.createElement('li');
@@ -89,4 +96,5 @@ function printWAF3() {
 
     let log = document.getElementById('WAF_log_text');
     log.append(...nodes);
+    log.scrollTo(0, 10000000); // y is very large to counter the buttons being pressed many times
 }
