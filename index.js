823ee5e58903859722006e562ddf61e0fc157fb0
TheCooliestGuy <mflavalley@gmail.com>
2021-12-16 21:34:28 -0500

    Added script, fixed some styling

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 // Import all plugins
 import * as bootstrap from 'bootstrap';
 
+document.getElementById("Attack1").addEventListener("click", printAttack1);
+
+function printAttack1() {
+    let tag = document.createElement("p");
+    let text = document.createTextNode(">Test");
+    tag.appendChild(text);
+    let log = document.getElementById('log_text');
+    log.appendChild(tag);
+}
+
+
+
+
+function printAttack1() {
+    let langs = ['Hacking into Mainframe','HTML','CSS'];
+
+    let nodes = langs.map(lang => {
+        let li = document.createElement('li');
+        li.textContent = lang;
+        return li;
+    });
+
+    //let text = document.createTextNode(">Test");
+    // tag.appendChild(text);
+    let log = document.getElementById('log_text');
+    log.append(...nodes);
+}
